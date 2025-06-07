6f70d1bd19f223e998e2bfa35a07ae482b6adfba
RaniyaNazrine <craniyanazrine321@gmail.com>
2025-06-07 08:36:03 +0530

    Add files via upload

---
+++ cast.py
@@ -0,0 +1,6 @@
+x=int(34.34)
+y=float(35)
+z=str(23.34)
+print(x)
+print(y)
+print(z)
---
+++ conversion.py
@@ -0,0 +1,14 @@
+x=1
+y=2j
+z=3.1
+
+a=float(x)
+b=complex(x)
+c=int(z)
+
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
